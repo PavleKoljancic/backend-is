7c71be71834b2b8721ca1c2fe9974ff80f104299
Pavle Koljancic <pavle.koljancic@student.etf.unibl.org>
2023-07-20 13:46:46 +0200

    Dodan Admin

--- src/main/java/com/app/backend/repositories/SupervisorWithPasswordRepo.java
+++ src/main/java/com/app/backend/repositories/SupervisorWithPasswordRepo.java
@@ -1,14 +1,11 @@
 package com.app.backend.repositories;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
-import com.app.backend.models.Supervisor;
 import com.app.backend.models.SupervisorWithPassword;
 import com.app.backend.models.UserWithPassword;
 
 public interface SupervisorWithPasswordRepo extends CrudRepository<SupervisorWithPassword,Integer> {
     
-        UserWithPassword findByEmail(String email);
+        SupervisorWithPassword findByEmail(String email);
 }
