be9e264a1e5eb07b5fca4f389b15b927c867a7ea
Pavle Koljancic <pavle.koljancic@student.etf.unibl.org>
2023-07-20 14:16:17 +0200

    TerminalActivationRequest

--- src/main/java/com/app/backend/repositories/AdminWithPasswordRepo.java
+++ src/main/java/com/app/backend/repositories/AdminWithPasswordRepo.java
@@ -3,8 +3,6 @@ package com.app.backend.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.app.backend.models.UserWithPassword;
-
 public interface AdminWithPasswordRepo extends CrudRepository<AdminWithPasswordRepo,Integer> {
     
         AdminWithPasswordRepo findByEmail(String email);
