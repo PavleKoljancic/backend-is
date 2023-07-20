06812bee31cd2c9950cbfe51d020114c53d3c8a9
Pavle Koljancic <pavle.koljancic@student.etf.unibl.org>
2023-07-20 14:16:01 +0200

    Ne znam vise sta sam radio

---
+++ src/main/java/com/app/backend/services/TerminalService.java
@@ -0,0 +1,25 @@
+package com.app.backend.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.backend.models.TerminalActivationRequest;
+import com.app.backend.repositories.TerminalActivationRequestRepo;
+
+
+
+@Service
+public class TerminalService {
+    private static final Integer Integer = null;
+    @Autowired
+    TerminalActivationRequestRepo terminalActivationRequestRepo;
+
+    public List<TerminalActivationRequest> findTerminalActivationRequestByTransporterId(Integer TRANSPORTER_Id) 
+    {
+        return terminalActivationRequestRepo.findByTRANSPORTERId(TRANSPORTER_Id);
+    }    
+
+    
+}
