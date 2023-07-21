f0992ebf76dc755d8f41346594ec41baef49ac98
Pavle Koljancic <pavle.koljancic@student.etf.unibl.org>
2023-07-21 13:54:44 +0200

    Dodan pregled supervizora po transporteru

--- src/main/java/com/app/backend/controllers/SupervisorController.java
+++ src/main/java/com/app/backend/controllers/SupervisorController.java
@@ -1,13 +1,17 @@
 package com.app.backend.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.backend.models.Supervisor;
 import com.app.backend.models.SupervisorWithPassword;
-import com.app.backend.models.UserWithPassword;
 import com.app.backend.services.SupervisorService;
 
 @RestController
@@ -22,4 +26,10 @@ public class SupervisorController {
         return supervisorService.registerSupervisor(supervisor);
     }
 
+    @GetMapping("/getByTransporterId={transporterId}")
+    public List<Supervisor> getSupervisorsByTransporterId(@PathVariable("transporterId") Integer transporterId) 
+     
+    {
+        return supervisorService.getSupervisorsByTransporterId(transporterId);
+    } 
 }
--- src/main/java/com/app/backend/services/SupervisorService.java
+++ src/main/java/com/app/backend/services/SupervisorService.java
@@ -1,10 +1,14 @@
 package com.app.backend.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.app.backend.models.Supervisor;
 import com.app.backend.models.SupervisorWithPassword;
+import com.app.backend.repositories.SupervisorRepo;
 import com.app.backend.repositories.SupervisorWithPasswordRepo;
 
 @Service
@@ -12,7 +16,8 @@ public class SupervisorService {
 
     @Autowired
 	private PasswordEncoder passwordEncoder;
-
+    @Autowired
+    SupervisorRepo supervisorRepo;
     @Autowired
     SupervisorWithPasswordRepo supervisorWithPasswordRepo;
 
@@ -22,4 +27,8 @@ public class SupervisorService {
         return supervisorWithPasswordRepo.save(supervisorWithPassword).getId();
         
     }
+
+        public List<Supervisor> getSupervisorsByTransporterId(Integer transporterId) {
+                return supervisorRepo.findByTransporterId(transporterId);
+        }
 }
