f4e374c3ef9abdc1ab83f51986b1941b9c15dc01
kezija99 <dejankezic99@gmail.com>
2023-07-25 11:24:34 +0200

    fixing last merge

--- src/main/java/com/app/backend/services/TicketRequestService.java
+++ src/main/java/com/app/backend/services/TicketRequestService.java
@@ -1,8 +1,10 @@
 package com.app.backend.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.app.backend.models.TicketRequest;
@@ -43,14 +45,29 @@ public class TicketRequestService {
         Integer resultId = ticketRequestRepo.addTicketRequest(userId, ticketTypeId);
         if(ticketType.getNeedsDocumentaion())
             return "Successfully added request";
+        
         TicketRequestResponse response = new TicketRequestResponse();
         response.setApproved(true);
         response.setComment("Automatska obrada");
         response.setTicketRequestId(resultId);
-        TicketRequestResponse dbResponse =  ticketRequestResponseRepo.save(response);
-        if(ticketRequestResponseRepo.processTicketResponse(dbResponse.getId()))
-            return "Ticket bought";
-        return "Ticket could not be bought";
+        
+        return addTicketResponse(response);
+    
+
     }
 
+    @Transactional
+    public String addTicketResponse(TicketRequestResponse response) 
+    {
+
+        TicketRequestResponse dbResponse =  ticketRequestResponseRepo.save(response);
+        if(ticketRequestResponseRepo.processTicketResponse(dbResponse.getId()))
+            return "Ticked Request Processed and Ticket bought";
+        return "Ticked Request Processed and wasn't Ticket bought";
+    }
+
+    public List<TicketRequest> getTicketRequestByTransporterId(Integer TRANSPORTER_Id, PageRequest pageRequest) 
+    {
+        return ticketRequestRepo.getTicketRequestByTransporterId(TRANSPORTER_Id);
+    }
 }
