addff3f5d67941ebfcfefcdc42068585eb128c73
Pavle Koljancic <pavle.koljancic@student.etf.unibl.org>
2024-04-11 20:51:52 +0200

    Kommit da bi se baza automatski inicijalizovala

--- src/main/java/com/app/backend/BackendApplication.java
+++ src/main/java/com/app/backend/BackendApplication.java
@@ -2,10 +2,18 @@ package com.app.backend;
 
 import java.math.BigDecimal;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 import com.app.backend.services.transactions.TransactionService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -16,5 +24,20 @@ public class BackendApplication {
 		scanTicketCost = TransactionService.getScanTransactionAmount();
 		SpringApplication.run(BackendApplication.class, args);
 	}
+    @Autowired
+    private DataSource dataSource;
+	@Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        DataSourceInitializer initializer = new DataSourceInitializer();
+		initializer.setEnabled(true);
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
 
+    private ResourceDatabasePopulator databasePopulator() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("schema.sql"));
+        return populator;
+    }
 }
